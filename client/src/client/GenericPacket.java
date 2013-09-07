5dd2ad60c8b978db75b439934a5442446f84435f
jfaghihnassiri <jfaghihnassiri@umail.ucsb.edu>
2013-09-07 09:56:00 -0700

    Added getters for the GenericPacket class, but left the internal variables public for now. Also added safety checks such as length must equal totalCommand.length and dataLength much equal commandData.length but for the time being these just print console error messages and continue execution.

--- client/src/client/GenericPacket.java
+++ client/src/client/GenericPacket.java
@@ -15,10 +15,12 @@ public class GenericPacket {
 	public byte[] dataLength; 			// 2 bytes
 	public byte[] commandData; 			// 0-2038 bytes
 	
+	// Invalid constructor
 	public GenericPacket(){
 		System.out.println("ERROR: Default constructor called on class Generic Packet");
 	}
 	
+	// Construct from continuous byte array and populate parts
 	public GenericPacket(byte[] totalInput){
 		controlSignals = Arrays.copyOfRange(totalInput, 0, 2);
 		commandType = Arrays.copyOfRange(totalInput, 2, 4);
@@ -28,11 +30,18 @@ public class GenericPacket {
 		
 		ByteBuffer wrapped = ByteBuffer.wrap(dataLength);
 		length = (int)wrapped.getShort() + 10;
+		if(length-10 != dataLength.length) {
+			System.out.println("ERROR: dataLength and length of commandData do not match");
+		}
 		
 		commandData = Arrays.copyOfRange(totalInput, 10, length);
 		totalCommand = Arrays.copyOfRange(totalInput, 0, length);
+		if(length != totalCommand.length) {
+			System.out.println("ERROR: length and totalCommand.length do not match");
+		}
 	}
 	
+	// Construct from parts of byte arrays and populate continuous
 	public GenericPacket(byte[] controlSignals,byte[] commandType,byte[] sequenceNumber,
 			byte[] acknowledgeNumber,byte[] dataLength,byte[] commandData) {
 		this.controlSignals = controlSignals;
@@ -44,6 +53,9 @@ public class GenericPacket {
 		
 		ByteBuffer wrapped = ByteBuffer.wrap(dataLength);
 		length = (int)wrapped.getShort() + 10;
+		if(length-10 != dataLength.length) {
+			System.out.println("ERROR: dataLength and length of commandData do not match");
+		}
 		
 		totalCommand = new byte[length];
 		ByteBuffer tmpTotal = ByteBuffer.wrap(totalCommand);
@@ -54,6 +66,31 @@ public class GenericPacket {
 		tmpTotal.put(dataLength);
 		if(length>10) tmpTotal.put(commandData);
 		totalCommand = tmpTotal.array();
+		if(length != totalCommand.length) {
+			System.out.println("ERROR: length and totalCommand.length do not match");
+		}
+	}
+	
+	// Getter for any of the byte arrays
+	public byte[] getByteItem(String name) {
+		if(name=="totalCommand") return totalCommand;
+		else if(name=="constrolSignals") return controlSignals;
+		else if(name=="commandType") return commandType;
+		else if(name=="sequenceNumber") return sequenceNumber;
+		else if(name=="acknowledgeNumber") return acknowledgeNumber;
+		else if(name=="dataLength") return dataLength;
+		else if(name=="commandData") return commandData;
+		else return null;
+	}
+	
+	// Getter for the length of the total command in bytes
+	public int getTotalLength() {
+		return length;
+	}
+	
+	// Getter for the length of the data command in bytes
+	public int getDataLength() {
+		return length-10;
 	}
 	
 }
