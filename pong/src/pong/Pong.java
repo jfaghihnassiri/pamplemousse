51d373297fc7c08e5be40ad9957d1151ed19a3f5
jfaghihnassiri <jfaghihnassiri@umail.ucsb.edu>
2013-09-08 22:35:06 -0700

    Added simple openGL code to populate a canvas with some static data, refresh at 60fps, and clear the frame and redraw it every time.

--- pong/src/pong/Pong.java
+++ pong/src/pong/Pong.java
@@ -1,10 +1,8 @@
 package pong;
 
-import org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.*;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
 
 public class Pong {
 
@@ -13,15 +11,41 @@ public class Pong {
 			Display.setDisplayMode(new DisplayMode(640,480));
 			Display.setTitle("Pong by PDM & JFN");
 			Display.create();
-		} catch (LWJGLException e) {
+		} 
+		catch (LWJGLException e) {
 			e.printStackTrace();
 		}
 		
 		// If openGL used initialize it here
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, 640, 480, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 		
 		while(!Display.isCloseRequested()) {
 			// Render
+			
+			// Clear before new frames
+			glClear(GL_COLOR_BUFFER_BIT); // if 3d bitwise with GL_DEPTH_BUFFER_BIT
+			
+			// Create a quadrilateral
+			glBegin(GL_QUADS);
+				glVertex2i(400,400); // Upper left
+				glVertex2i(450,400); // Upper right
+				glVertex2i(450,450); // Bottom right
+				glVertex2i(400,450); // Bottom left
+			glEnd();
+			
+			// Create a line
+			glBegin(GL_LINES);
+				glVertex2i(100,100);
+				glVertex2i(200,200);
+			glEnd();
+				
+			// Send changes?
 			Display.update();
+			
+			// Determine FPS
 			Display.sync(60);
 		}
 		
